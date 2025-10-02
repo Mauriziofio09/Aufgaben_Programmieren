266ca62c691294291c774d3af7a48f38fef04639
mauriziofiore <maurizio_fiore@sluz.ch>
2025-10-02 10:01:06 +0200

    Update Nr.1

---
+++ Aufgabe17/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("\nGib dein Geburtsdatum ein (Format: TT.MM.JJJJ): ");
+        string eingabe = Console.ReadLine();
+
+        if (DateTime.TryParse(eingabe, out DateTime geburtsdatum))
+        {
+            DateTime heute = DateTime.Today;
+
+            int jahre = heute.Year - geburtsdatum.Year;
+            int monate = heute.Month - geburtsdatum.Month;
+            int tage = heute.Day - geburtsdatum.Day;
+
+            if (tage < 0)
+            {
+                monate--;
+                DateTime letzterMonat = heute.AddMonths(-1);
+                tage += DateTime.DaysInMonth(letzterMonat.Year, letzterMonat.Month);
+            }
+
+            if (monate < 0)
+            {
+                jahre--;
+                monate += 12;
+            }
+
+            TimeSpan gesamt = heute - geburtsdatum;
+            int wochen = gesamt.Days / 7;
+
+            Console.WriteLine($"Alter in Jahren: {jahre}");
+            Console.WriteLine($"Alter in Monaten : {monate}");
+            Console.WriteLine($"Alter in Wochen : {wochen}");
+            Console.WriteLine($"Alter in Tagen : {tage}");
+        } 
+    }
+}
---
+++ Aufgabe18/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Bitte einen Text eingeben:");
+        string input = Console.ReadLine();
+
+        char[] vokale = { 'a', 'e', 'i', 'o', 'u', 'ä', 'ö', 'ü', 
+            'A', 'E', 'I', 'O', 'U', 'Ä', 'Ö', 'Ü' };
+
+        Dictionary<char, int> zaehlung = new Dictionary<char, int>();
+
+        foreach (char v in vokale)
+        {
+            zaehlung[v] = 0;
+        }
+
+        foreach (char c in input)
+        {
+            if (zaehlung.ContainsKey(c))
+            {
+                zaehlung[c]++;
+            }
+        }
+
+        int total = 0;
+        foreach (var item in zaehlung)
+        {
+            total += item.Value;
+        }
+
+        Console.WriteLine("\nErgebnisse:");
+        Console.WriteLine("Gesamtanzahl Vokale: " + total);
+
+        foreach (var item in zaehlung)
+        {
+            if (item.Value > 0)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+        }
+
+        Console.WriteLine("\nDrücke eine Taste zum Beenden...");
+        Console.ReadKey();
+    }
+}
